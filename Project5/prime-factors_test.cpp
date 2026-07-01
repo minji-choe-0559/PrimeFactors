d5bf873c87c1c1be05288d44c3e2352105f39298
minji-choe-0559 <mia827.oh@emcore.kr>
2026-07-01 09:47:27 +0900

    [refactor] use test fixture for PrimeFactors tests

--- Project5/prime-factors_test.cpp
+++ Project5/prime-factors_test.cpp
@@ -1,40 +1,52 @@
 #include "gmock/gmock.h"
 #include "prime-factors.cpp"
 
-TEST(PrimeFactors, Of1) {
-	PrimeFactors primeFactors;
-	std::vector<int>expected = {};
-	EXPECT_EQ(expected, primeFactors.of(1));
+using namespace testing;
+
+class PrimeFactorsFixture : public Test {
+protected:
+	void SetUp() override {}
+	void TearDown() override {}
+
+	void runTest(int num) {
+		EXPECT_EQ(expected, primeFactors.of(num));
+	}
+
+	static PrimeFactors primeFactors;
+	static std::vector <int> expected;
+};
+
+PrimeFactors PrimeFactorsFixture::primeFactors;
+std::vector<int> PrimeFactorsFixture::expected;
+
+TEST_F(PrimeFactorsFixture, Of1) {
+	expected = {};
+	runTest(1);
 }
 
-TEST(PrimeFactors, Of2) {
-	PrimeFactors primeFactors;
-	std::vector<int>expected = { 2 };
-	EXPECT_EQ(expected, primeFactors.of(2));
+TEST_F(PrimeFactorsFixture, Of2) {
+	expected = { 2 };
+	runTest(2);
 }
 
-TEST(PrimeFactors, Of3) {
-	PrimeFactors primeFactors;
-	std::vector<int>expected = { 3 };
-	EXPECT_EQ(expected, primeFactors.of(3));
+TEST_F(PrimeFactorsFixture, Of3) {
+	expected = { 3 };
+	runTest(3);
 }
 
-TEST(PrimeFactors, Of4) {
-	PrimeFactors primeFactors;
-	std::vector<int>expected = { 2, 2 };
-	EXPECT_EQ(expected, primeFactors.of(4));
+TEST_F(PrimeFactorsFixture, Of4) {
+	expected = { 2, 2 };
+	runTest(4);
 }
 
-TEST(PrimeFactors, Of5) {
-	PrimeFactors primeFactors;
-	std::vector<int>expected = { 5 };
-	EXPECT_EQ(expected, primeFactors.of(5));
+TEST_F(PrimeFactorsFixture, Of5) {
+	expected = { 5 };
+	runTest(5);
 }
 
-TEST(PrimeFactors, Of6Of7) {
-	PrimeFactors primeFactors;
-	std::vector<int>expected = { 2, 3 };
-	EXPECT_EQ(expected, primeFactors.of(6));
+TEST_F(PrimeFactorsFixture, Of6Of7) {
+	expected = { 2, 3 };
+	runTest(6);
 	expected = { 7 };
-	EXPECT_EQ(expected, primeFactors.of(7));
+	runTest(7);
 }
