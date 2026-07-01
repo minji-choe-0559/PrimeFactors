83f26b5c217d902f5aefc11202f1c76b6652a794
minji-choe-0559 <mia827.oh@emcore.kr>
2026-07-01 09:20:06 +0900

    [feature] Implement prime factorization

--- Project5/prime-factors.cpp
+++ Project5/prime-factors.cpp
@@ -1,4 +1,21 @@
+#include <vector>
+
 class PrimeFactors {
 public:
+	std::vector <int>& of(int num) {
+        for (int i = 2; i * i <= num; ++i)
+        {
+            while (num % i == 0)
+            {
+                result.push_back(i);
+                num /= i;
+            }
+        }
+
+        if (num > 1)
+            result.push_back(num);
+		return result;
+	}
 private:
+	std::vector <int> result;
 };
--- Project5/prime-factors_test.cpp
+++ Project5/prime-factors_test.cpp
@@ -1,6 +1,32 @@
 #include "gmock/gmock.h"
 #include "prime-factors.cpp"
 
-TEST(PrimeFactors, sample) {
-	EXPECT_EQ(0, 0);
+TEST(PrimeFactors, Of1) {
+	PrimeFactors primeFactors;
+	std::vector<int>expected = {};
+	EXPECT_EQ(expected, primeFactors.of(1));
+}
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactors primeFactors;
+	std::vector<int>expected = { 2 };
+	EXPECT_EQ(expected, primeFactors.of(2));
+}
+
+TEST(PrimeFactors, Of3) {
+	PrimeFactors primeFactors;
+	std::vector<int>expected = { 3 };
+	EXPECT_EQ(expected, primeFactors.of(3));
+}
+
+TEST(PrimeFactors, Of4) {
+	PrimeFactors primeFactors;
+	std::vector<int>expected = { 2, 2 };
+	EXPECT_EQ(expected, primeFactors.of(4));
+}
+
+TEST(PrimeFactors, Of5) {
+	PrimeFactors primeFactors;
+	std::vector<int>expected = { 5 };
+	EXPECT_EQ(expected, primeFactors.of(5));
 }
